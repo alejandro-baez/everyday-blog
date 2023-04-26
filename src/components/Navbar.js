a509cfb955c2c3e8fcb95fa7641b72022d28d121
Alejandro Baez <abaez2623@gmail.com>
2023-04-25 22:32:05 -0400

    Added a logo and linked it to home, added styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,25 +1,28 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 //need to use link
 
 const Navbar = () => {
   return (
     <header className="header-container">
-        <div className='logo container-item'>Logo
-        </div>
-        <div className='nav-options container-item'>
-            <ul className='nav-list-links'>
-                <li>Home</li>
-                <li>Search</li>
-                <li>Log In</li>
-                <li>Sign Up</li>
-
-            </ul>
-        </div>
-
+      <div className="logo container-item">
+        <Link to="/">
+          <img
+            src="https://banner2.cleanpng.com/20201021/tix/transparent-blog-icon-writing-icon-social-media-icon-5f90eb01928963.5926112316033328656002.jpg"
+            alt=""
+          />
+        </Link>
+      </div>
+      <div className="nav-options container-item">
+        <ul className="nav-list-links">
+          <li>Home</li>
+          <li>Search</li>
+          <li>Log In</li>
+          <li>Sign Up</li>
+        </ul>
+      </div>
     </header>
-  )
-}
+  );
+};
 
-export default Navbar
-
+export default Navbar;
